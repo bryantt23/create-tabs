95dd801a2e2243d9a0e1f57ac8a43da58509fb54
bryant tunbutr <bryantt23@users.noreply.github.com>
2022-08-10 11:34:56 -0700

    styling with change handler

--- src/App.js
+++ src/App.js
@@ -1,29 +1,44 @@
 import './App.css';
 import { useState } from 'react';
 
+const selectedButtonStyle = {
+  backgroundColor: '#0089f2',
+  color: 'white',
+  borderRadius: 5,
+  padding: '0 10px',
+  height: 40,
+  border: '1px solid #0089f2',
+  margin: 2
+};
+
+const nonSelectedButtonStyle = {
+  backgroundColor: 'white',
+  color: '#0089f2',
+  borderRadius: 5,
+  padding: '0 10px',
+  height: 40,
+  border: '1px solid white',
+  margin: 2
+};
+
 function App() {
   const [selectedTab, setSelectedTab] = useState('simple');
   return (
     <div className='App'>
       <button
-        style={{
-          backgroundColor: '#0089f2',
-          color: 'white',
-          borderRadius: 5,
-          padding: '10px 20px'
-        }}
+        style={
+          selectedTab === 'simple'
+            ? selectedButtonStyle
+            : nonSelectedButtonStyle
+        }
         onClick={() => setSelectedTab('simple')}
       >
         Simple
       </button>
       <button
-        style={{
-          color: '#0089f2',
-          backgroundColor: 'white',
-          borderRadius: 5,
-          padding: '10px 20px',
-          border: '1px white solid'
-        }}
+        style={
+          selectedTab === 'fancy' ? selectedButtonStyle : nonSelectedButtonStyle
+        }
         onClick={() => setSelectedTab('fancy')}
       >
         <span style={{ fontWeight: 800 }}>Fancy</span> title
